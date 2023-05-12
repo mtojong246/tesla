6039e13f39392bbe93a82a6dbff663a8a215f955
Melanie <mtojong@live.unc.edu.email>
2023-05-12 12:17:13 -0400

    flex

--- src/Components/Footer/footer.component.jsx
+++ src/Components/Footer/footer.component.jsx
@@ -3,7 +3,7 @@ import './footer.styles.scss';
 const Footer = () => {
     return (
         <div className='footer-container' id='footer' >
-            <div className='footer-options' style={{display: `${window.innerWidth > 625 ? 'block' : 'none'}`}}>
+            <div className='footer-options' style={{display: `${window.innerWidth > 625 ? 'flex' : 'none'}`}}>
                 <span>Tesla &#169; 2023</span>
                 <span>Privacy & Legal</span>
                 <span>Vehicle Recalls</span>
